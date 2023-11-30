56e7bdbd362e49d2383369ff68b5f14ddc6998aa
Jason Kai <tkai@uwo.ca>
2023-11-30 15:16:50 -0500

    Add RSVP button, add container

--- app/components/engagement.tsx
+++ app/components/engagement.tsx
@@ -1,19 +1,25 @@
 import engagementStyles from "../styles/engagement.module.css";
 
-import Row from "react-bootstrap/Row";
+import { Button, Row } from "react-bootstrap";
 
 const EngagementComponent = () => {
   return (
     <div className={engagementStyles.main}>
-      <Row className={engagementStyles.text}>
-        Please join us to celebrate the engagement of
-      </Row>
-
-      <Row className={engagementStyles.names}>Lauren & Jason</Row>
-
-      <Row className={engagementStyles.text}>TBD</Row>
-      <br />
-      <Row className={engagementStyles.text}>Somewhere over the rainbow</Row>
+      <div className={engagementStyles.container}>
+        <Row className={engagementStyles.text}>
+          Please join us to celebrate the engagement of
+        </Row>
+        <Row className={engagementStyles.names}>Lauren & Jason</Row>
+        <Row className={engagementStyles.text}>TBD</Row>
+        <br />
+        <Row className={engagementStyles.text}>Somewhere over the rainbow</Row>
+        <br />
+        <Button className={engagementStyles.btn}>
+          <a href="#" target="_blank">
+            RSVP
+          </a>
+        </Button>
+      </div>
     </div>
   );
 };
