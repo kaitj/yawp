927599d88d3a91acec6b560712b46d90f3d8dbc7
Jason Kai <tkai@uwo.ca>
2023-11-30 19:11:18 -0500

    Add confetti

--- app/components/effects.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "../styles/effects.module.css";
-
-const GoldFlakeBackdrop = () => {
-  return <div className={styles.goldFlakeBackdrop}></div>;
-};
-
-export default GoldFlakeBackdrop;
--- app/components/engagement.tsx
+++ app/components/engagement.tsx
@@ -1,21 +1,35 @@
 import engagementStyles from "../styles/engagement.module.css";
 
+import Image from "next/image";
 import { Button, Row } from "react-bootstrap";
+
+import confettiImg from "../../public/photos/gold-sparkles.png";
 
 const EngagementComponent = () => {
   return (
     <div className={engagementStyles.main}>
+      <div className={engagementStyles.confettiContainer}>
+        <Image
+          className={engagementStyles.confetti}
+          src={confettiImg}
+          alt="Confetti"
+        />
+      </div>
+
       <div className={engagementStyles.container}>
         <Row className={engagementStyles.text}>
           Please join us to celebrate the engagement of
         </Row>
         <Row className={engagementStyles.names}>Lauren & Jason</Row>
-        <Row className={engagementStyles.text}>TBD</Row>
+        <Row className={engagementStyles.text}>December 29, 2023</Row>
         <br />
-        <Row className={engagementStyles.text}>Somewhere over the rainbow</Row>
+        <Row className={engagementStyles.text}>
+          Evans Room @ Glenerin Inn & Spa <br />
+          1695 The Collegeway, Mississauga, Ontario
+        </Row>
         <br />
         <Button className={engagementStyles.btn}>
-          <a href="#" target="_blank">
+          <a href="https://forms.gle/oDLw1G1DmDCs6CdY6" target="_blank">
             RSVP
           </a>
         </Button>
