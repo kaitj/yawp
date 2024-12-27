468b784fd7cdcb5d1347e00e51942c7c5af6c06f
Jason Kai <21226986+kaitj@users.noreply.github.com>
2024-12-27 18:00:02 -0500

    Update post event

--- app/components/engagement.tsx
+++ app/components/engagement.tsx
@@ -1,7 +1,7 @@
 import engagementStyles from "../styles/engagement.module.css";
 
 import Image from "next/image";
-import { Button, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 import confettiImg from "../../public/photos/gold-sparkles.png";
 
@@ -20,17 +20,17 @@ const EngagementComponent = () => {
         <Row
           className={`${engagementStyles.text} ${engagementStyles.welcomeText}`}
         >
-          Please join us to celebrate the engagement of
+          Thanks for joining us to celebrate the engagement of
         </Row>
         <Row className={engagementStyles.names}>Lauren & Jason</Row>
-        <Row className={engagementStyles.text}>December 29, 2023</Row>
-        <br />
-        <Row className={engagementStyles.text}>
-          Glenerin Inn & Spa @ 6:30pm <br />
-          1695 The Collegeway, Mississauga, Ontario
-        </Row>
-        <br />
-        <Row className={engagementStyles.text}>(Attire: Smart Casual)</Row>
+        {/* <Row className={engagementStyles.text}>December 29, 2023</Row> */}
+        {/* <br /> */}
+        {/* <Row className={engagementStyles.text}> */}
+        {/* Glenerin Inn & Spa @ 6:30pm <br /> */}
+        {/* 1695 The Collegeway, Mississauga, Ontario */}
+        {/* </Row> */}
+        {/* <br /> */}
+        {/* <Row className={engagementStyles.text}>(Attire: Smart Casual)</Row>
         <Button className={engagementStyles.btn}>
           <a
             href="https://withjoy.com/laurensmith-and-jasonkai/rsvp"
@@ -38,7 +38,7 @@ const EngagementComponent = () => {
           >
             RSVP
           </a>
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
