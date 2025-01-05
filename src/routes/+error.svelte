448927e4b6937fbff4a21f9909b17d56b9f92c0b
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-01-05 14:56:11 -0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex flex-col justify-center items-center">
+	<div class="w-full max-w-2xl mx-auto mb-8">
+		<img src="/images/finn_error.jpg" alt="Finn in snow" class="mx-auto" />
+	</div>
+
+	<div class="w-full max-w-4xl mx-auto mb-8 text-black text-center">
+		<h2>{$page.status}: {$page.error.message} <br /> Here is a picture of Finn instead</h2>
+	</div>
+</div>
