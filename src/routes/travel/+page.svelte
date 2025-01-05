d2cd3607e827dfb2397f5f0a2c43fe206ddc91fc
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-01-05 15:38:59 -0500

    Add wip travel page

---
+++ src/routes/travel/+page.svelte
@@ -0,0 +1,41 @@
+<div class="flex flex-col text-center justify-center items-center">
+	<!-- Center image -->
+	<div class="w-full max-w-2xl mb-8">
+		<img src="/images/travel/finn.jpeg" alt="Save the date" class="w-4/5 mx-auto" />
+	</div>
+
+	<div class="w-full max-w-4xl mx-auto mb-8 text-black">
+		<h2 class="h2 p-6 md:text-4xl xs:text-md text-tertiary-300">To Mexico</h2>
+		<div
+			class="w-full max-w-4xl grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-8 text-black text-center"
+		>
+			<!-- Details -->
+			<div class="md:text-lg xs:text-md">
+				<h3>From Toronto</h3>
+				<p class="text-left">[DETAILS]</p>
+			</div>
+
+			<!-- Vertical divider -->
+			<div class="hidden md:block w-px bg-tertiary-300"></div>
+
+			<!-- Forms -->
+			<div class="md:text-lg xs:text-md my-auto">
+				<h3>From Elsewhere</h3>
+				<p class="text-left">[DETAILS]</p>
+			</div>
+		</div>
+		<br />
+		<p class="text-justify md:text-lg xs:text-md">
+			While there is an airport in Tulum, it is <strong>recommended</strong> to fly into Cancún International
+			Airport (CUN) due to the availablity of flights.
+		</p>
+
+		<h2 class="h2 p-6 md:text-4xl xs:text-md text-tertiary-300">To Resort</h2>
+		<p class="text-justify md:text-lg xs:text-md">
+			The resort is approximately 75 minutes from Cancún International Airport (CUN). There will be
+			one of two options for getting to the resort. If you arrived with the wedding party (from
+			Toronto), a bus for the wedding party will be waiting! If you arrived from elsewhere, a
+			shuttle transfer will be arranged!
+		</p>
+	</div>
+</div>
